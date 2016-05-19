b37b749ea92b36fb004296db182705af8d0f3ab7
Clint Fralick <cfralick@creeperengine.com>
2016-05-19 13:55:46 -0400

    remove sessionm

--- static/js/fb.js
+++ static/js/fb.js
@@ -1,1 +1,23 @@
+;(function(window, document, undefined) {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : 'your-app-id',
+        status     : true,
+        xfbml      : true,
+        version    : 'v2.5'
+      });
 
+      FB.Event.subscribe('auth.statusChange', function(response) {
+        console.log(response.status);
+      });
+    };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+})(window, document, undefined);
